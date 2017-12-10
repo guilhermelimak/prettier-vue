5671f06604bc29847cce5cb71a298ffb09605978
Guilherme Lima <guilhermelimak@gmail.com>
2017-12-10 07:10:24 -0200

    test: Add regexIndexOf tests

--- __tests__/util/reges-index-of.spec.js
+++ __tests__/util/reges-index-of.spec.js
@@ -1,4 +1,15 @@
 const regexIndexOf = require('../../src/utils/regex-index-of')
 
-/** TODO: Write tests for this */
-it('should do something')
+it('should return the index of matching regex', () => {
+  const txt = '<style>testText000'
+
+  expect(regexIndexOf(txt, /sty.*>/gi)).toEqual(1)
+  expect(regexIndexOf(txt, /test/gi)).toEqual(7)
+})
+
+it('should return the last index of matching regex', () => {
+  const txt = '<style>testText000'
+
+  expect(regexIndexOf(txt, /sty.*>/gi, true)).toEqual(7)
+  expect(regexIndexOf(txt, /test/gi, true)).toEqual(11)
+})
